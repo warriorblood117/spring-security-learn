005c2656fb30e9fecefec8ef5cbda584f43748b2
warriorblood117 <warriorblood117@gmail.com>
2024-05-25 23:53:32 -0500

    create a simple AuthenticationManager that use a DaoUthenticationProvider for use a userDetailsService

--- src/main/java/com/mateo/springsecuritybasic/configuration/SecurityConfig.java
+++ src/main/java/com/mateo/springsecuritybasic/configuration/SecurityConfig.java
@@ -1,10 +1,22 @@
 package com.mateo.springsecuritybasic.configuration;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,13 +25,50 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request -> {
-            request.requestMatchers("/public/**").permitAll();
-            request.anyRequest().authenticated();
-        })
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/public/**").permitAll();
+                    request.requestMatchers(HttpMethod.POST, "/public/login").permitAll();
+                    request.anyRequest().authenticated();
+                })
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails mateo = User.withDefaultPasswordEncoder()
+                .username("mateo")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+        UserDetails luzma = User.withDefaultPasswordEncoder()
+                .username("luz")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+        List<UserDetails> users = List.of(mateo, luzma);
+        return new InMemoryUserDetailsManager(users);
+
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
--- src/main/java/com/mateo/springsecuritybasic/controllers/HelloController.java
+++ src/main/java/com/mateo/springsecuritybasic/controllers/HelloController.java
@@ -1,12 +1,24 @@
 package com.mateo.springsecuritybasic.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("/public/hello")
     public String helloGet() {
@@ -17,6 +29,58 @@ public class HelloController {
     public String helloGetPrivate() {
         return "helo private GET";
     }
-    
+
+    @GetMapping("/")
+    public String hello(){
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+        String p = context.getAuthentication().getDetails().toString();
+        return "Bienvenido " + username + p;
+    }
+
+    @PostMapping("/public/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) {
+        try {
+            // Autenticar al usuario utilizando el AuthenticationManager
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+
+                    
+            
+            // Si la autenticación es exitosa, devolver un mensaje de éxito
+            return ResponseEntity.ok("Inicio de sesión exitoso");
+        } catch (Exception e) {
+            // Si la autenticación falla, devolver un mensaje de error
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error de autenticación: " + e.getMessage());
+        }
+    }
+
+    public static class AuthRequest {
+        private String username;
+        private String password;
+
+        public AuthRequest(String username,String password){
+            this.username = username;
+            this.password = password;
+        }
+
+        public AuthRequest(){}
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+    }
     
 }
